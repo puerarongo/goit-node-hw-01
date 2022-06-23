7ad5fa92bcb6f0e22f3aca101a2e8d6755212b1c
puerarongo <akhardin96@gmail.com>
2022-06-23 14:47:41 +0300

    add addContacts func

--- contacts.js
+++ contacts.js
@@ -1,33 +1,64 @@
-const fs = require("fs").promises;
+const fs = require("fs");
 const path = require("path");
 const contactsPath = path.resolve("./db/contacts.json");
 
-fs.readFile(contactsPath).then(data => console.log(data.toString())).catch(error => console.error(error))
 
 //! function 
 async function listContacts() {
     console.log("listContacts")
-    try {
-        const response = await fs.readFile(contactsPath);
-        return response.toString();
-    }
-    catch (error) {
-        console.error(error);
-    };
+    fs.readFile(contactsPath, "utf-8", (error, data) => {
+        if (error) {
+            console.error(error);
+        }
+        return console.log(data);
+    });
 };
 
-function getContactById(contactId) {
-  // ...твой код
-}
+function getContactById(id) {
+    console.log("getContactById");
+    fs.readFile(contactsPath, "utf-8", (error, data) => {
+        if (error) {
+            console.error(error);
+        }
+        const found = JSON.parse(data).filter(elem => elem.id === id.toString());
+        if (found.length < 1) {
+            return console.log("Sorry, this person is not in the contact database!");
+        }
+        return console.log(found);
+    });
+};
 
-function removeContact(contactId) {
-  // ...твой код
-}
+//function removeContact(contactId) {
+//  console.log("removeContact")
+//  fs.readFile(contactsPath, "utf-8").then(data => {
+//    const found = JSON.parse(data).filter(elem => elem.id === id.toString())
+//    if (found.length < 1) {
+//      return console.log("Sorry, this person is not in the contact database!");
+//    }
+//
+//  });
+//}
+
 
 function addContact(name, email, phone) {
-  // ...твой код
+    fs.readFile(contactsPath, "utf-8", (error, data) => {
+        if (error) {
+            console.error(error);
+        }
+        const coincidence = JSON.parse(data).filter(elem => elem.name === name.toString())
+        console.log(coincidence)
+        if (coincidence.length >= 1) {
+            return console.log("This name already exists in the database!")
+        }
+        const dataContact = [...JSON.parse(data), { name, email, phone }]
+        fs.writeFile(contactsPath, JSON.stringify(dataContact), () => {
+            return listContacts();
+        })
+    });
 };
 
-listContacts();
+//listContacts();
+//getContactById(2);
+addContact("Alex Cross", "across@mail.com", "222-22-22")
 
-module.exports = { listContacts };
+//module.exports = { listContacts };
